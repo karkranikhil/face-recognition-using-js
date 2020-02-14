954cb75509d1c7ec2c92896012628288d08f92b8
nikhil karkra <karkra.nikhil@gmail.com>
2020-02-14 14:25:57 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ const isScreenSmall = window.matchMedia("(max-width: 700px)");
 let predictedAges = [];
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("../models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("../../models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
   faceapi.nets.faceExpressionNet.loadFromUri("../models"),
   faceapi.nets.ageGenderNet.loadFromUri("../models")
